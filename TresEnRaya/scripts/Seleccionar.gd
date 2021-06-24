442513d63e564730a606fdb56ff68eb598590103
brecabre <bravomantonio@gmail.com>
2021-06-24 22:54:34 +0200

    Add files via upload

--- TresEnRaya/scripts/Seleccionar.gd
+++ TresEnRaya/scripts/Seleccionar.gd
@@ -8,18 +8,12 @@ onready var ClienteTextEditSERVER_IP = get_node("Cliente/TextEditSERVER_IP")
 onready var ClienteTextEditSERVER_PORT = get_node("Cliente/TextEditSERVER_PORT")
 onready var ServerTextEditSERVER_PORT = get_node("Server/TextEditSERVER_PORT")
 onready var LabelResultados = get_node("LabelResult")
-
+var mostraRresultado = " "
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	ClienteTextEditSERVER_IP.set_text("127.0.0.1")
 	ClienteTextEditSERVER_PORT.set_text("1909")
 	ServerTextEditSERVER_PORT.set_text("1909")
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 
 func _on_Cliente_pressed():
@@ -27,21 +21,20 @@ func _on_Cliente_pressed():
 	GlobalServer.jugador = "cliente"
 	var SERVER_IP = ClienteTextEditSERVER_IP.get_text() 
 	var SERVER_PORT = int(ClienteTextEditSERVER_PORT.get_text())
-	var resultado = ""
+	mostraRresultado = "Conectando con el servidor"
 	GlobalServer.create_client(SERVER_IP, SERVER_PORT)
-	PrintarResult(resultado)
-	
-	pass # Replace with function body.
+	PrintarResult(mostraRresultado)
 	
 
 
 func _on_Server_pressed():
 	desactivarBotones()
 	GlobalServer.jugador = "Server"
-	var SERVER_PORT = int(ClienteTextEditSERVER_PORT.get_text())
+	
+	var SERVER_PORT = int(ServerTextEditSERVER_PORT.get_text())
 	GlobalServer.create_server(SERVER_PORT)
-	var resultado = "Esperando al cliente"
-	PrintarResult(resultado)
+	mostraRresultado = "Esperando al cliente"
+	PrintarResult(mostraRresultado)
 
 func PrintarResult(resultado):
 	LabelResultados.set_text(resultado)
